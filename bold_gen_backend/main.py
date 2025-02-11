378d4e2fb817bd6dea153e54af70bfca04454b03
Muhammad-Masood <development.masood@gmail.com>
2025-02-11 11:44:24 +0500

    update origin

--- bold_gen_backend/main.py
+++ bold_gen_backend/main.py
@@ -5,9 +5,15 @@ from .routers import landlord, buyer, contractor, tennat, auth
 from .internal import admin
 from typing import Annotated
 from fastapi.middleware.cors import CORSMiddleware
+import os
+from dotenv import load_dotenv
+
+_:bool = load_dotenv()
+origin_client_https = os.getenv("ORIGIN_CLIENT_HTTPS")
 
 origins = [
     "http://localhost:3000",
+    origin_client_https,
 ]
 
 @asynccontextmanager
